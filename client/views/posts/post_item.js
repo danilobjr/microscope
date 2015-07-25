fe208bd4b184a1a1e22cb717538ae88881b5543f
danilojrr <danilobjr@gmail.com>
2015-07-25 11:49:22 -0300

    removing comments

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -2,7 +2,6 @@ Template.postItem.helpers({
     domain: function() {
         var anchor = document.createElement('a');
         anchor.href = this.url;
-        console.log(anchor.hostname);
         return anchor.hostname;
     }
 });
